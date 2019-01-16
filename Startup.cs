50b3e42db4764ca170e0959909a970233a827833
Kent B Carlyle II <kentbcarlyle@gmail.com>
2019-01-15 21:00:04 -0500

    add some lower and higher level middleware for understanding concepts

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace OdeToFood
 {
@@ -20,12 +21,36 @@ namespace OdeToFood
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter)
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter, ILogger<Startup> logger)
     {
-      if (env.IsDevelopment())
+      // if (env.IsDevelopment())
+      // {
+      //   app.UseDeveloperExceptionPage();
+      // }
+
+      app.Use(next =>
       {
-        app.UseDeveloperExceptionPage();
-      }
+        return async context =>
+        {
+          logger.LogInformation("Request Incoming");
+
+          if (context.Request.Path.StartsWithSegments("/mym"))
+          {
+            await context.Response.WriteAsync("Hit!");
+            logger.LogInformation("Request Handled");
+          }
+          else
+          {
+            await next(context);
+            logger.LogInformation("Response Outgoing");
+          }
+        };
+      });
+
+      app.UseWelcomePage(new WelcomePageOptions
+      {
+        Path = "/wp"
+      });
 
       app.Run(async (context) =>
       {
